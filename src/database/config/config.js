2ca820fecebc535d722eaa6c10f10059b2c69073
Robert Odhiambo <odhizrober254@gmail.com>
2024-11-20 00:49:36 +0300

    first commit

---
+++ src/database/config/config.js
@@ -0,0 +1,21 @@
+const dotenv = require("dotenv");
+const pg = require("pg");
+dotenv.config();
+
+module.exports = {
+  development: {
+    url: process.env.LOCAL_DB_URL,
+    dialect: 'postgres',
+    dialectModule: pg,
+  },
+  test: {
+    url: process.env.LOCAL_DB_URL,
+    dialect: 'postgres',
+    dialectModule: pg,
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectModule: pg,
+  },
+};
